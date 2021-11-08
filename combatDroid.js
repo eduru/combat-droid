451ca9d3a57fb8b9604f63fa537b1524274ea0f3
Eduardo Ruiz <eduru@me.com>
2021-11-08 13:06:32 +0900

    create some  helpers

--- combatDroid.js
+++ combatDroid.js
@@ -1,23 +1,32 @@
-class TargetSelectionModule {
-  constructor() {}
+const { test, test2 } = require("./helpers.js");
 
-  setTarget(params) {
-    let target = [];
-    if (!params) throw new Error("Invalid command");
-    const { protocols, scan } = params;
-    protocols.forEach((e) => {
-      if (e === "closest-enemies") target.push("closest-enemies");
-      if (e === "furthest-enemies") target.push("furthest-enemies");
-      if (e === "assist-allies") target.push("assist-allies");
-      if (e === "avoid-crossfire") target.push("avoid-crossfire");
-      if (e === "prioritize-mech") target.push("prioritize-mech");
-      if (e === "avoid-mech") target.push("avoid-mech");
-    });
-    console.log(target);
-  }
-}
+test();
+test2();
 
-const testDroid = new TargetSelectionModule();
-testDroid.setTarget({ protocols: ["closest-enemies", "avoid-mech"] });
+// class TargetSelectionModule {
+//   constructor() {}
+
+//   setTarget(params) {
+//     const { protocols, scan } = params;
+
+//     if (!params) throw new Error("Invalid command");
+
+//     let target = {};
+//     let instructions = [...scan];
+
+//     protocols.forEach((e) => {
+//       if (e === "closest-enemies") target.push("closest-enemies");
+//       if (e === "furthest-enemies") target.push("furthest-enemies");
+//       if (e === "assist-allies") target.push("assist-allies");
+//       if (e === "avoid-crossfire") target.push("avoid-crossfire");
+//       if (e === "prioritize-mech") target.push("prioritize-mech");
+//       if (e === "avoid-mech") target.push("avoid-mech");
+//     });
+//     console.log(target);
+//   }
+// }
+
+// const testDroid = new TargetSelectionModule();
+// testDroid.setTarget({ protocols: ["closest-enemies", "avoid-mech"] });
 
 //module.exports = CombatDroid;
---
+++ helpers.js
@@ -0,0 +1,11 @@
+const test = () => {
+  console.log("test1");
+};
+const test2 = () => {
+  console.log("test2");
+};
+
+module.exports = {
+  test,
+  test2,
+};
